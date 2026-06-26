a3983c13a85720ca53dac951aae43887818705fa
Tinyiko Ndou <St10490357@rcconnect.edu.za>
2026-06-26 16:34:09 +0200

    Activity Log added and modifief

--- cyberSecurityChaTBottGUI/ActivityLog.cs
+++ cyberSecurityChaTBottGUI/ActivityLog.cs
@@ -1,4 +1,4 @@
-﻿/*using System;
+﻿using System;
 using System.Collections.Generic;
 
 namespace cyberSecurityChaTBottGUI
@@ -56,4 +56,4 @@ namespace cyberSecurityChaTBottGUI
             return result;
         }
     }
-}*/
+}
